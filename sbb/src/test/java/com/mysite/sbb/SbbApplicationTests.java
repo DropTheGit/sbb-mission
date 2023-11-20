0141a2b6d00f471d533a3104e1cbfe03ffce1c53
Sanghyuk Park <dropthegit@gmail.com>
2023-11-21 00:33:29 +0900

    2.05  Repository Done

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,17 +21,39 @@ class SbbApplicationTests {
     private AnswerRepository answerRepository;
 
     @Test
-	void testJpa(){
+	@Transactional
+	// Transactional 어노테이션이 없다면 LazyInitializationException이 발생함.
+	// 이는 Question 객체를 조회한 후 DB세션이 끊어지기 때문인데, 실제 서버에서는 JPA 프로그램 들을 실행할 때는 DB 세션이 종료되지 않기 때문에 이는 테스트 코드에서만 발생
+	// Transactional 어노테이션은 메서드가 종료될 때까지 DB 세션을 유지한다
+	void testJpa() {
 		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);  // 어떤 질문에 대한 답변인지 알기 위해 Question 객체 연결
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
+
+//	void testJpa(){
+//		Optional<Answer> oa = this.answerRepository.findById(1);
+//		assertTrue(oa.isPresent());
+//		Answer a = oa.get();
+//		assertEquals(2, a.getQuestion().getId());
+//	}
+
+//	void testJpa(){
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//
+//		Answer a = new Answer();
+//		a.setContent("네 자동으로 생성됩니다.");
+//		a.setQuestion(q);  // 어떤 질문에 대한 답변인지 알기 위해 Question 객체 연결
+//		a.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(a);
+//	}
 
 //	void testJpa() {
 //		assertEquals(2, this.questionRepository.count());
