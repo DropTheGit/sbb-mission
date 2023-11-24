7a3278b17bda7352daf4eb5cc511104afe64ca5e
Sanghyuk Park <dropthegit@gmail.com>
2023-11-24 14:53:13 +0900

    3-02 Paging

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,6 +2,7 @@ package com.mysite.sbb;
 
 import com.mysite.sbb.Question.Question;
 import com.mysite.sbb.Question.QuestionRepository;
+import com.mysite.sbb.Question.QuestionService;
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import jakarta.transaction.Transactional;
@@ -19,109 +20,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class SbbApplicationTests {
 
     @Autowired
-    private QuestionRepository questionRepository;
-    @Autowired
-    private AnswerRepository answerRepository;
+    private QuestionService questionService;
 
     @Test
-	@Transactional
-	// Transactional 어노테이션이 없다면 LazyInitializationException이 발생함.
-	// 이는 Question 객체를 조회한 후 DB세션이 끊어지기 때문인데, 실제 서버에서는 JPA 프로그램 들을 실행할 때는 DB 세션이 종료되지 않기 때문에 이는 테스트 코드에서만 발생
-	// Transactional 어노테이션은 메서드가 종료될 때까지 DB 세션을 유지한다
-	void testJpa() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
-	}
-
-//	void testJpa(){
-//		Optional<Answer> oa = this.answerRepository.findById(1);
-//		assertTrue(oa.isPresent());
-//		Answer a = oa.get();
-//		assertEquals(2, a.getQuestion().getId());
-//	}
-
-//	void testJpa(){
-//		Optional<Question> oq = this.questionRepository.findById(2);
-//		assertTrue(oq.isPresent());
-//		Question q = oq.get();
-//
-//		Answer a = new Answer();
-//		a.setContent("네 자동으로 생성됩니다.");
-//		a.setQuestion(q);  // 어떤 질문에 대한 답변인지 알기 위해 Question 객체 연결
-//		a.setCreateDate(LocalDateTime.now());
-//		this.answerRepository.save(a);
-//	}
-
-//	void testJpa() {
-//		assertEquals(2, this.questionRepository.count());
-//		Optional<Question> oq = this.questionRepository.findById(1);
-//		assertTrue(oq.isPresent());
-//		Question q = oq.get();
-//		this.questionRepository.delete(q);
-//		assertEquals(1, this.questionRepository.count());
-//	}
-
-//	void testJpa() {
-//		Optional<Question> oq = this.questionRepository.findById(1);
-//		assertTrue(oq.isPresent());
-//		Question q = oq.get();
-//		q.setSubject("수정된 제목");
-//		this.questionRepository.save(q);
-//	}
-
-//	void testJpa(){
-//		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-//		Question q = qList.get(0);
-//		assertEquals("sbb가 무엇인가요?", q.getSubject());
-//	}
-
-//
-//	void testJpa(){
-//		Question q = this.questionRepository.findBySubjectAndContent(
-//				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-//		assertEquals(1, q.getId());
-//	}
-
-//	void testJpa(){
-//		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-//		assertEquals(1, q.getId());
-//	}
-
-//	void testJpa(){
-//		Optional<Question> oq = this.questionRepository.findById(1);
-//		if(oq.isPresent()){
-//			Question q = oq.get();
-//			assertEquals("sbb가 무엇인가요?", q.getSubject());
-//		}
-//	}
-//
-
-
-//	void testJpa(){
-//		List<Question> all = this.questionRepository.findAll();
-//		assertEquals(2, all.size());
-//
-//		Question q = all.get(0);
-//		assertEquals("sbb가 무엇인가요?", q.getSubject());
-//	}
-
-//	void testJpa() {
-//		Question q1 = new Question();
-//		q1.setSubject("sbb가 무엇인가요?");
-//		q1.setContent("sbb에 대해서 알고 싶습니다.");
-//		q1.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q1); // 첫번째 질문 저장
-//
-//		Question q2 = new Question();
-//		q2.setSubject("스프링부트 모델 질문입니다.");
-//		q2.setContent("id는 자동으로 생성되나요?");
-//		q2.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q2); // 두번째 질문 저장
-//	}
+    void testJpa() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
+    }
 }
